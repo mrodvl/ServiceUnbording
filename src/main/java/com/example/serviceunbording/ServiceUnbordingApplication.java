e100d32902c55697ae02d4aa9c21c9cdfb2f6d2d
Snekold <e89030066143@yandex.ru>
2025-04-16 21:25:27 +0300

    addh

--- src/main/java/com/example/serviceunbording/ServiceUnbordingApplication.java
+++ src/main/java/com/example/serviceunbording/ServiceUnbordingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceUnbordingApplication {
-    //Egor
+    //Egor add
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceUnbordingApplication.class, args);
